50507ea9b171f101a39453286888bbaeb3cb996a
Jan Dudulski <jan@dudulski.pl>
2018-11-15 18:25:10 +0100

    Chapter 2, Task 4
    And apply mix formater

--- functional-chapter-02/task02/task02.exs
+++ functional-chapter-02/task02/task02.exs
@@ -1,4 +1,6 @@
 distance = 200
 time = 4
 
-IO.puts "Bob traveled #{distance} km in #{time} hours which gives #{Float.round(distance / time, 2)} km/h average velocity."
+IO.puts(
+  "Bob traveled #{distance} km in #{time} hours which gives #{Float.round(distance / time, 2)} km/h average velocity."
+)

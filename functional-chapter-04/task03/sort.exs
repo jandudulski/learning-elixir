7f068820dee74d5eb0b566ae159be303c31c70f5
Jan Dudulski <jan@dudulski.pl>
2018-11-26 22:07:59 +0100

    Add naive QuickSort for fun

--- functional-chapter-04/task03/sort.exs
+++ functional-chapter-04/task03/sort.exs
@@ -20,3 +20,15 @@ end
 IO.inspect Sort.descending([9, 5, 1, 5, 4])
 IO.inspect Sort.descending([2, 2, 3, 1])
 IO.inspect Sort.descending(["c", "d", "a", "c"])
+
+defmodule QSort do
+  def qs([]), do: []
+  def qs([a]), do: [a]
+  def qs([hd | tl]) do
+    qs(for x <- tl, x <= hd, do: x) ++ [hd] ++ qs(for x <- tl, x > hd, do: x)
+  end
+end
+
+IO.inspect QSort.qs([9, 5, 1, 5, 4])
+IO.inspect QSort.qs([2, 2, 3, 1])
+IO.inspect QSort.qs(["c", "d", "a", "c"])

40e9e21911a039539b9fc07f7f1506a9d6e450f3
Jan Dudulski <jan@dudulski.pl>
2018-12-10 21:23:20 +0100

    Advent of Code - Day 8, part 2

--- advent-08/lib/day8.ex
+++ advent-08/lib/day8.ex
@@ -36,7 +36,7 @@ defmodule Day8 do
     {[], result} =
       file_name
       |> load_file()
-      |> sum_of_metadata()
+      |> sum_of_children()
 
     result
   end
@@ -48,14 +48,49 @@ defmodule Day8 do
     |> Enum.map(&String.to_integer/1)
   end
 
+  def sum_of_children([]), do: {[], 0}
+
+  def sum_of_children([0, metadata | tail]) do
+    tail
+    |> Enum.split(metadata)
+    |> (fn {head, tail} ->
+          {tail, head |> Enum.sum()}
+        end).()
+  end
+
+  def sum_of_children([children, metadata | tree]) do
+    {tail, children} =
+      1..children
+      |> Enum.reduce({tree, []}, fn _child, {tail, acc} ->
+        {tail, sum} = sum_of_children(tail)
+        {tail, [sum | acc]}
+      end)
+
+    children = Enum.reverse(children)
+
+    tail
+    |> Enum.split(metadata)
+    |> (fn {head, tail} ->
+          subsum =
+            head
+            |> Enum.reduce(0, fn child_no, acc ->
+              acc + Enum.at(children, child_no - 1, 0)
+            end)
+
+          {tail, subsum}
+        end).()
+  end
+
   def sum_of_metadata([]), do: {[], 0}
+
   def sum_of_metadata([0, metadata | tail]) do
     tail
     |> Enum.split(metadata)
     |> (fn {head, tail} ->
-      {tail, head |> Enum.sum()}
-    end).()
+          {tail, head |> Enum.sum()}
+        end).()
   end
+
   def sum_of_metadata([children, metadata | tree]) do
     {tail, subsum} =
       1..children
@@ -67,7 +102,7 @@ defmodule Day8 do
     tail
     |> Enum.split(metadata)
     |> (fn {head, tail} ->
-      {tail, subsum + Enum.sum(head)}
-    end).()
+          {tail, subsum + Enum.sum(head)}
+        end).()
   end
 end

c6f30ec0a2affb7f298b0e162be6c1ec8fcb991c
Jan Dudulski <jan@dudulski.pl>
2018-12-10 12:10:18 +0100

    Advent of Code - Day 8, part 1

--- advent-08/lib/day8.ex
+++ advent-08/lib/day8.ex
@@ -4,15 +4,70 @@ defmodule Day8 do
   """
 
   @doc """
-  Hello world.
+  Give answer for part 1.
 
   ## Examples
 
-      iex> Day8.hello()
-      :world
+      iex> Day8.part1("example.txt")
+      138
 
   """
-  def hello do
-    :world
+  @spec part1(binary) :: integer
+  def part1(file_name) do
+    {[], result} =
+      file_name
+      |> load_file()
+      |> sum_of_metadata()
+
+    result
+  end
+
+  @doc """
+  Give answer for part 2.
+
+  ## Examples
+
+      iex> Day8.part2("example.txt")
+      66
+
+  """
+  @spec part2(binary) :: integer
+  def part2(file_name) do
+    {[], result} =
+      file_name
+      |> load_file()
+      |> sum_of_metadata()
+
+    result
+  end
+
+  def load_file(file_name) do
+    file_name
+    |> File.read!()
+    |> String.split([" ", "\n"], trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def sum_of_metadata([]), do: {[], 0}
+  def sum_of_metadata([0, metadata | tail]) do
+    tail
+    |> Enum.split(metadata)
+    |> (fn {head, tail} ->
+      {tail, head |> Enum.sum()}
+    end).()
+  end
+  def sum_of_metadata([children, metadata | tree]) do
+    {tail, subsum} =
+      1..children
+      |> Enum.reduce({tree, 0}, fn _child, {tail, acc} ->
+        {tail, sum} = sum_of_metadata(tail)
+        {tail, acc + sum}
+      end)
+
+    tail
+    |> Enum.split(metadata)
+    |> (fn {head, tail} ->
+      {tail, subsum + Enum.sum(head)}
+    end).()
   end
 end
--- advent-08/test/day8_test.exs
+++ advent-08/test/day8_test.exs
@@ -1,8 +1,4 @@
 defmodule Day8Test do
   use ExUnit.Case
   doctest Day8
-
-  test "greets the world" do
-    assert Day8.hello() == :world
-  end
 end
